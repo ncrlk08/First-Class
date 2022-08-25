a90e492bb41fa3994bc1356c89e72bc18437b525
ncrlk <ncrlk@DESKTOP-5TELBKM.T-mobile.com>
2022-08-25 10:39:02 -0400

    Operator practice

---
+++ src/mypack/Operators.java
@@ -0,0 +1,20 @@
+
+package mypack;
+
+public class Operators {
+	
+	public static void main (String[] args) {
+		int i = 10;
+		int j = 20;
+		
+		System.out.println(i+j);
+		System.out.println(i-j);
+		System.out.println(i*j);
+		System.out.println(i/j);
+		System.out.println(i%j);
+		
+		 
+		
+
+}
+}
